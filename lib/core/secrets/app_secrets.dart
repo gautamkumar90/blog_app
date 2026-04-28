c17f420470c89209c307f54955d1f9e4cca13070
Gautam Kumar <gautam.gk004@gmail.com>
2026-04-28 21:19:04 +0530

    Initial Commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = "https://ziqtspzhiiacaofqgqjw.supabase.co";
+  static const anonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppcXRzcHpoaWlhY2FvZnFncWp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3OTcxMDYsImV4cCI6MjA5MjM3MzEwNn0.BAdDKEWvdEZOHFDDkxzvgF9qwLgtaMrHW58zKR28Y8M";
+}
--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -34,56 +34,58 @@ class _SignupPageState extends State<SignupPage> {
     formKey.currentState?.validate();
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Sign Up",
-                style: TextStyle(
-                  color: AppPallete.whiteColor,
-                  fontSize: 46,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30),
-              AuthField(hintText: "Name", controller: nameController),
-              const SizedBox(height: 15),
-              AuthField(hintText: "Email", controller: emailController),
-              const SizedBox(height: 15),
-              AuthField(
-                hintText: "Password",
-                controller: passwordController,
-                isObscure: true,
-              ),
-              const SizedBox(height: 20),
-              const AuthGradientButton(buttonText: "Sign Up"),
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context, LoginPage.route());
-                },
-                child: RichText(
-                  text: TextSpan(
-                    text: "Already have an account? ",
-                    style: Theme.of(context).textTheme.titleMedium,
-                    children: [
-                      TextSpan(
-                        text: "Sign in",
-                        style: Theme.of(context).textTheme.titleMedium
-                            ?.copyWith(
-                              color: AppPallete.gradient2,
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    color: AppPallete.whiteColor,
+                    fontSize: 46,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 30),
+                AuthField(hintText: "Name", controller: nameController),
+                const SizedBox(height: 15),
+                AuthField(hintText: "Email", controller: emailController),
+                const SizedBox(height: 15),
+                AuthField(
+                  hintText: "Password",
+                  controller: passwordController,
+                  isObscure: true,
+                ),
+                const SizedBox(height: 20),
+                const AuthGradientButton(buttonText: "Sign Up"),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, LoginPage.route());
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Already have an account? ",
+                      style: Theme.of(context).textTheme.titleMedium,
+                      children: [
+                        TextSpan(
+                          text: "Sign in",
+                          style: Theme.of(context).textTheme.titleMedium
+                              ?.copyWith(
+                                color: AppPallete.gradient2,
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
