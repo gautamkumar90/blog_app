110e6eb30b1201d43d5744927d4b173fb40e584b
Gautam Kumar <gautam.gk004@gmail.com>
2026-05-06 22:41:45 +0530

    Usecase, bloc, exceptions added

---
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final UserSignUp _userSignUp;
+
+  AuthBloc({required UserSignUp userSignUp})
+    : _userSignUp = userSignUp,
+      super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      emit(AuthLoading());
+
+      //we are basically calling the usecase here and passing the params to it
+      //and we can call it in this way because we have implemented the call method in the usecase and we are passing the params to it. 
+      final res = await _userSignUp( 
+        UserSignUpParams(
+          name: event.name,
+          email: event.email,
+          password: event.password,
+        ),
+      );
+
+      res.fold(
+        (failure) => emit(AuthFailure(failure.message)),
+        (uid) => emit(AuthSuccess(uid)),
+      );
+    });
+  }
+}
